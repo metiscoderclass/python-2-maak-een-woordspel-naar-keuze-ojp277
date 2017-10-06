1b2c9de1f3e5a088bb86fceabbc20e85ad01fc36
Otto Plasmeijer <ottojohan.plasmeijer@coderclass.nl>
2017-10-06 09:06:04 +0200

    een klein beging aan galgje

---
+++ galgje.py
@@ -0,0 +1,15 @@
+import random
+import math
+import time
+
+print("Welkom bij Galgje!")
+print ("  ___    __    __    ___   ____  ____ ")
+print (" / __)  /__\  (  )  / __) (_  _)( ___)")
+print ("( (_-. /(__)\  )(__( (_-..-_)(   )__) ")
+print (" \___/(__)(__)(____)\___/\____) (____)")
+print("")
+
+while True:
+    woorden=["waterleiding","poepen","school","deurklink","balk"]
+
+    print ("bibb" + random.choise(woorden))
